1d4c4b6862322254c6fde021e5fdb25d6ff67981
章生 <me@jiluo.cc>
2023-07-20 22:39:40 +0800

    feat: prettier 1.0.2

--- packages/prettier-config/source/index.ts
+++ packages/prettier-config/source/index.ts
@@ -1,86 +1,30 @@
-import { type Config } from "prettier";
+import { type Config } from 'prettier'
 
-// const printWidth: Config["printWidth"] = 80;
-
-// const tabWidth: Config["tabWidth"] = 2;
-
-// const useTabs: Config["useTabs"] = false;
-
-// 不添加非必要分号
-export const semi: Config["semi"] = false;
-
-// 使用单引号
-export const singleQuote: Config["singleQuote"] = true;
-
-// const quoteProps: Config["quoteProps"] = "as-needed";
-
-// const jsxSingleQuote: Config["jsxSingleQuote"] = false;
-
-// const trailingComma: Config["trailingComma"] = "all";
-
-// const bracketSpacing: Config["bracketSpacing"] = true; 
-
-// const bracketSameLine: Config["bracketSameLine"] = false;
-
-// 尽可能省略箭头函数括号
-export const arrowParens: Config["arrowParens"] = "avoid";
-
-// const rangeStart: Config["rangeStart"] = 0;
-
-// const rangeEnd: Config["rangeEnd"] = Infinity;
-
-// const parser: Config["parser"];
-
-// const filepath: Config["filepath"];
-
-// const requirePragma: Config["requirePragma"] = false; 
-
-// const insertPragma: Config["insertPragma"] = false;
-
-// 修改 Markdown 样式
-export const proseWrap: Config["proseWrap"] = "never";
-
-// 忽略 HTML 中空白字符
-export const htmlWhitespaceSensitivity: Config["htmlWhitespaceSensitivity"] = "ignore";
-
-// const vueIndentScriptAndStyle: Config["vueIndentScriptAndStyle"] = false;
-
-// const endOfLine: Config["endOfLine"] = "lf";
-
-// const embeddedLanguageFormatting: Config["embeddedLanguageFormatting"] = "auto";
-
-// const singleAttributePerLine: Config["singleAttributePerLine"] = false;
-
-export const overrides: Config["overrides"] = [
-  {
-    files: ["*.css", "*.less", "*.scss", "*.sass"],
-    options: {
-      singleQuote: false,
+const config: Config = {
+  // 不添加非必要分号
+  semi: false,
+  // 使用单引号
+  singleQuote: true,
+  // 修改 Markdown 样式
+  proseWrap: 'never',
+  // 尽可能省略箭头函数括号
+  arrowParens: 'avoid',
+  // 忽略 HTML 中空白字符
+  htmlWhitespaceSensitivity: 'ignore',
+  overrides: [
+    {
+      files: ['*.css', '*.less', '*.scss', '*.sass'],
+      options: {
+        singleQuote: false,
+      },
     },
-  },
-];
+  ],
+}
 
-// export default {
-//   // printWidth,
-//   // tabWidth,
-//   // useTabs,
-//   semi,
-//   singleQuote,
-//   // quoteProps,
-//   // jsxSingleQuote,
-//   // trailingComma,
-//   // bracketSpacing,
-//   // bracketSameLine,
-//   arrowParens,
-//   // rangeStart,
-//   // rangeEnd,
-//   // requirePragma,
-//   // insertPragma,
-//   proseWrap,
-//   htmlWhitespaceSensitivity,
-//   // vueIndentScriptAndStyle,
-//   // endOfLine,
-//   // embeddedLanguageFormatting,
-//   // singleAttributePerLine,
-//   overrides,
-// };
+export const {
+  semi,
+  singleQuote,
+  proseWrap,
+  htmlWhitespaceSensitivity,
+  overrides,
+} = config
